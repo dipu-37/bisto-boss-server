65d70a6180df7516a32e1a693ee832e650975541
dipu-37 <razibul1105nstu@gmail.com>
2024-10-13 13:47:16 +0600

    stripe Payment method

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-
+// This is your test secret API key.
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000
 const app = express()
 const jwt = require('jsonwebtoken');
@@ -213,7 +214,21 @@ async function run() {
       res.send(result);
     })
 
+    // payment intent 
 
+    app.post("/create-payment-intent", async (req, res) => {
+       const {price} = req.body;
+       const amount = parseInt(price * 100);
+       const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency : 'usd',
+        payment_method_types:['card']
+       });
+       res.send({
+        clientSecret: paymentIntent.client_secret
+       })
+
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
